02196a68d1b48d4d0c4f1293a67b6b8009480781
StathisLiontis <stathisliontis@gmail.com>
2026-05-30 10:38:46 +0300

    ping-pong.py
    v1.1

--- ping-pong/ping-pong.py
+++ ping-pong/ping-pong.py
@@ -25,16 +25,26 @@ class Player_Role(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 400:
             self.rect.y += 5
 
+class Ball_Role(GameSprite):
+    def update(self):
+        pass
+
 #create game window
 window = display.set_mode((700, 500))
 display.set_caption('Ping-Pong_two_players')
 
-#background
+#background and items
 backgroynd_game = transform.scale(image.load("table_ping_pong_champion_vs_pong_star.png"),(700, 500))
 player_1 = Player_Role("racketbulep.png", 600, 250, 5)
 player_2 = Player_Role("racketredss.png", 30, 250, 5)
+ball = Ball_Role("ball_champions.png", 250, 250, 5)
+
+#ball directions
+speed_x = 3
+speed_y = 3
 
 #music for background
+
 #mixer.init()
 #mixer.music.load("")
 #mixer.music.play()
@@ -45,18 +55,31 @@ FPS = 60
 
 # game worked but now the name is run
 run = True
+finish = False
 while run:
     window.blit(backgroynd_game,(0,0))
     keys_pressed = key.get_pressed()
     player_1.reset()
     player_2.reset()
-    
+    ball.reset()
+
     for e in event.get():
         if e.type == QUIT:
             run = False
     
     player_1.update()
     player_2.updates()
+    #ball.update()
     
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    
+    if ball.rect.y < 0:
+        speed_y *= -1
+    
+    if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+        speed_x *= -1
+
     clock.tick(FPS)
-    display.update()
+    display.update() 
