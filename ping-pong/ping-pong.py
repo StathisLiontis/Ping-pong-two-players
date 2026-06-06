12d8d2cde6e07832a4ce5a1bd15bfc53878ec40d
Stathis <stathisliontis@gmail.com>
2026-06-06 17:01:56 +0300

    ping-pong.py
    v2.3

--- ping-pong/ping-pong.py
+++ ping-pong/ping-pong.py
@@ -40,7 +40,8 @@ ball = Ball_Role("ball_champions.png", 250, 250, speed_of_ball, 50, 50)
 win_height = 500
 # text font 
 font.init()
-style = font.SysFont("Arial", 40)
+style = font.SysFont("Arial", 34)
+styles = font.SysFont("Arial", 25)
 #ball directions and perfect score
 speed_x = 5
 speed_y = 5
@@ -55,6 +56,7 @@ mixer.music.set_volume(0.5)
 mixer.music.play()
 sound_of_winner = mixer.Sound('kevincsupo-marble-it-up-ultra-soccer-win-sound.mp3')
 restart_sound = mixer.Sound('restart_button.mp3')
+pause_sound = mixer.Sound('pause_sound_effect.mp3')
 #clock for FPS
 clock = time.Clock()
 FPS = 60 
@@ -63,7 +65,10 @@ run = True
 finish = False
 sound_played = False
 sound_played_restart = False
+sound_pause = False
 pause = False
+How_play = False
+music_muted = False
 while run:
     window.blit(backgroynd_game,(0,0))
     keys_pressed = key.get_pressed()
@@ -98,31 +103,71 @@ while run:
                 player_2.rect.y = 250
                 #επαναφορα τις μουσικης
                 mixer.music.play(-1)
+        #mute the music
+        if e.type == KEYDOWN:
+            if e.key == K_m:
+                if music_muted:
+                    mixer.music.unpause()
+                    music_muted = False
+                else:
+                    mixer_music.pause()
+                    music_muted = True
+        # h for learn how to play the game.
+        if e.type == KEYDOWN:
+            if e.key == K_h:
+                How_play = not How_play
+                sound_pause = False
         # space for use pause
         if e.type == KEYDOWN:
             if e.key == K_SPACE and not finish:
                 pause = not pause
-    # pause and unpause
-    if pause:
-        pause_text = style.render("Press 'SPACE' to UNpause the game", True,(255, 219, 61))
-        window.blit(pause_text,(115, 100))
+                sound_pause = False
+    # how the players play the game.
+    if How_play:
+        How_play_text_1 = style.render('Player1 (red racket) use W and S', True, (225, 219, 61))
+        window.blit(How_play_text_1, (35, 100))
+        How_play_text_1_1 = style.render('in the keyboard to move', True, (225, 219, 61))
+        window.blit(How_play_text_1_1, (35, 140))
+        How_play_text_2 = style.render('Player2 (blue racket) use UP and DOWN Arrows', True, (225, 219, 61))
+        window.blit(How_play_text_2, (35, 170))
+        How_play_text_2_2 = style.render('in the keyboard to move', True, (225, 219, 61))
+        window.blit(How_play_text_2_2, (35, 200))
+        How_play_text_2_3 = style.render("Press 'H' to play", True, (225, 219, 61))
+        window.blit(How_play_text_2_3, (50, 250))
+        if not sound_pause:
+            pause_sound.play()
+            sound_pause = True
         display.update()
         clock.tick(FPS)
         continue
-    
+    # pause and unpause
+    if pause:
+        pause_text = style.render("Press 'SPACE' to UNpause the game", True,(255, 219, 61))
+        window.blit(pause_text,(125, 100))
+        if not sound_pause:
+            pause_sound.play()
+            sound_pause = True
+        display.update()
+        clock.tick(FPS)
+        continue
     #text for match
     text_match_score = style.render(f'P1: {score_player_1} | P2: {score_player_2}', 1, (255,255,255))
-    window.blit(text_match_score,(10, 450))
+    window.blit(text_match_score,(3, 455))
     # score perfect saves of ball
-    text_score = style.render("perfect saves of ball:" + str(score), 1, (225, 255, 255))
+    text_score = style.render("Perfect saves of Ball:" + str(score), 1, (225, 255, 255))
     window.blit(text_score,(10, 2))
     # informaition for space
     informaition_text = style.render("Press 'SPACE' to pause", True,(255, 219, 61))
-    window.blit(informaition_text,(350, 2))
-
-    # lose for player 2 and winner player 1
+    window.blit(informaition_text,(360, 2))
+    # information for mute the music
+    mute_music_text = styles.render("Press 'M' mute/unmute music", True,(255, 219, 61))
+    window.blit(mute_music_text, (410, 460))
+    #information for how to play by button edition
+    informaition_play_text = styles.render("Press 'H' how to play", True, (255, 219, 61))
+    window.blit(informaition_play_text, (160, 470))
+    # lose for player 2 and winner player 1 
     if ball.rect.x > 650:
-        if score_player_1 <= 3:
+        if score_player_1 <= 2:
             score_player_1 += 1 # take poit player 1
         else:
             score_player_1 += 0
@@ -140,18 +185,18 @@ while run:
                 sound_played = True
             # lose text
             lose = style.render("You lost the ball player 2", True, (0, 230, 255))
-            window.blit(lose, (175, 90))
+            window.blit(lose, (200, 90))
             # winner text
             winner = style.render("The player 1 is the winner", True, (255, 0, 0))
-            window.blit(winner, (175, 130))
+            window.blit(winner, (190, 130))
             # restart text
             restart_text = style.render("Press 'R' to Restart the game", True,(255, 219, 61))
-            window.blit(restart_text,(175, 200))
+            window.blit(restart_text,(205, 200))
             finish = True
     
     # lose for player 1 and winner player 2
     if ball.rect.x < -4:
-        if score_player_2 <= 3:
+        if score_player_2 <= 2:
             score_player_2 += 1 # take poit player 2
         else:
             score_player_2 += 0
@@ -169,13 +214,13 @@ while run:
                 sound_played = True
             # lose text
             lose = style.render("You lost the ball player 1", True, (255, 0, 0))
-            window.blit(lose, (175, 90))
+            window.blit(lose, (200, 90))
             # winner text
             winners = style.render("The player 2 is the winner", True, (0, 230, 255))
-            window.blit(winners, (175, 130))
+            window.blit(winners, (190, 130))
             # restart text
             restart_text = style.render("Press 'R' to Restart the game", True,(255, 219, 61))
-            window.blit(restart_text,(175, 200))
+            window.blit(restart_text,(205, 200))
             finish = True
     
     # finish and rules
